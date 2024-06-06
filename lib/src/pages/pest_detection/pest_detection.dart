5a568fda5cc74a4349a7050e5ea0a2975a6e6eee
Manu <manu1998kj@gmail.com>
2024-06-06 17:54:57 +0100

    feat: Added FertilizerRecommendation and PestDetection pages

---
+++ lib/src/pages/pest_detection/pest_detection.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class PestDetection extends StatelessWidget {
+  const PestDetection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
