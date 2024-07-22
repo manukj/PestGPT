1a3038ea05ab83d0a4c51d2c7b49b711dce62699
Manu <manu1998kj@gmail.com>
2024-07-22 18:23:46 +0100

    refactor: Improve image detection by optimizing bounding box drawing
    The code changes refactor the `ImageUtil` class to improve the image detection functionality by optimizing the drawing of bounding boxes. Instead of using a fixed red color for all bounding boxes, the code now uses a list of predefined colors to assign a unique color to each bounding box. This enhances the visual representation of the detected objects and makes it easier to distinguish between different objects. The changes also include the addition of a `getColor` method to retrieve the color based on the index of the bounding box. Overall, these improvements enhance the accuracy and usability of the image detection feature.

--- lib/src/utils/image_util.dart
+++ lib/src/utils/image_util.dart
@@ -2,10 +2,27 @@ import 'package:image/image.dart' as img;
 import 'package:pest_gpt/src/models/pest_detection/pest_detection_response.dart';
 
 class ImageUtil {
+  static List pestColors = [
+    img.ColorRgb8(255, 0, 0),
+    img.ColorRgb8(0, 255, 0),
+    img.ColorRgb8(0, 0, 255),
+    img.ColorRgb8(255, 0, 255),
+    img.ColorRgb8(192, 192, 192),
+    img.ColorRgb8(128, 128, 128),
+    img.ColorRgb8(128, 0, 0),
+    img.ColorRgb8(0, 255, 255),
+    img.ColorRgb8(255, 255, 0),
+  ];
+
+  static img.Color getColor(int index) {
+    return pestColors[index % pestColors.length];
+  }
+
   static void drawBoundingBoxes(
       img.Image image, PestDetectionResponse response) {
     for (var detectionList in response.items) {
-      for (var item in detectionList) {
+      for (int i = 0; i < detectionList.length; i++) {
+        var item = detectionList[i];
         List<double> box = item.box;
         List<int> originalShape = item.originalShape;
 
@@ -18,22 +35,20 @@ class ImageUtil {
         int y1 = (box[1] * yScale).toInt();
         int x2 = (box[2] * xScale).toInt();
         int y2 = (box[3] * yScale).toInt();
-        // Draw the bounding box
-        drawRect(
-            image, x1, y1, x2, y2, img.ColorRgb8(255, 0, 0)); // Red color box
-      }
-    }
-  }
+        var color = getColor(i);
 
-  static void drawRect(
-      img.Image image, int x1, int y1, int x2, int y2, img.Color color) {
-    for (int x = x1; x <= x2; x++) {
-      image.setPixel(x, y1, color);
-      image.setPixel(x, y2, color);
-    }
-    for (int y = y1; y <= y2; y++) {
-      image.setPixel(x1, y, color);
-      image.setPixel(x2, y, color);
+        img.drawString(image, item.className,
+            font: img.arial14, x: (x1), y: y1, color: img.ColorRgb8(0, 0, 0));
+
+        img.drawRect(image,
+            x1: x1,
+            y1: y1,
+            x2: x2,
+            y2: y2,
+            color: color,
+            thickness: 10,
+            radius: 4); // Red color box
+      }
     }
   }
 }
